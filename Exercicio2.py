e12b212e73e53303fd759032a709fb10bdb37cdb
Angélica <titahcarvalho@hotmail.com>
2024-09-10 11:40:19 -0300

    Atualizações com os exercicios

--- Exercicio2.py
+++ Exercicio2.py
@@ -28,3 +28,26 @@ print(f"Frase em minúsculas: {frase.lower()}")
 print(f"Número de caracteres na frase: {len(frase)}")
 
 print(f"Frase com cada palavra capitalizada: {frase.title()}")
+
+#EXERCÍCIOS MÓDULO 4
+#FaÇa um programa que solicite o ano de nascimento do usu´ario e calcule sua idade (considere o ano atual como 2023)
+ano_nascimento = int(input("Digite o ano de seu nascimento:"))
+idade = 2024 - ano_nascimento
+print(f"Você tem {idade}anos.")
+
+# Escreva um programa que pe¸ ca o raio de um c´ırculo ao usu´ario e imprima sua ´area (use pi = 3.14159).
+raio = float(input("Digite o raio de uma circuferencia em cm:"))
+pi = 3.14159
+area = pi*(raio**2)
+print(f"A area dessa circuferencia é: {area}")
+
+#Faça um programa que solicite um número inteiro e imprima seu antecessor e seu sucessor.
+numero = int(input("Digite um número:"))
+ant = numero - 1
+suc = numero + 1
+print("O antecessor de", numero,"é:",ant, "\nO sucessor de", numero,"é:", suc)
+#10. Escreva um programa que pe¸ ca o sal´ario base de um funcion´ario e calculeo sal´ario l´ ıquido, considerando um desconto de 10% para impostos.
+salario = float(input("Digite o seu salario bruto:"))
+liquido = salario - (salario * 0.1)
+
+print(f"O seu salario liquido é: R${liquido:.2f}")
